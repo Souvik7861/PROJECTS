e717fe1bc21000fa1f74cef25fb1f59079bbdc97
Souvik7861 <120063616+Souvik7861@users.noreply.github.com>
2024-01-15 02:08:22 +0530

    Add files via upload

---
+++ PySpark EMR Airflow: Real Estate Transformations/ingest.sh
@@ -0,0 +1,1 @@
+wget -O - https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_tracker/city_market_tracker.tsv000.gz | aws s3 cp - s3://pyspark-emr-airflow-yml/raw-data-yml/city_market_tracker.tsv000.gz
